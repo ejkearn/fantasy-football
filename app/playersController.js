2a90798dcc978be8f31fc52f3e72cd0b650d32f8
Edward Kearn <ejkearn@gmail.com>
2018-05-03 16:47:10 -0600

    draws players to screen after search

--- app/playersController.js
+++ app/playersController.js
@@ -4,17 +4,35 @@ function PlayersController() {
     var playersService = new PlayersService(ready);
 
     function ready() {
-        
+
         loading = false;
 
     }
-function drawPlayers(players){
-    
-}
+    function drawPlayers(players) {
+        var template = '';
+        for (var i = 0; i < players.length; i++) {
+            var player = players[i];
+            console.log(player.fullname)
+            template += `<div class="col-2">
+            <div class="card" class="">
+            <img class="card-img-top" src="${player.photo}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${player.fullname}</h5>
+                <p class="card-text">Postion: ${player.position}</p>
+                <p class="card-text">team: ${player.position}</p>
+                <a href="#" class="btn btn-primary">ADD</a>
+            </div>
+            </div>
+            </div>`
+        }
+        var face = 'face'
+        // console.log(template)
+        document.getElementById('players').innerHTML = template;
+    }
     //Public
-    this.search = function searchName(e){
-        debugger
+    this.searchName = function searchName(e) {
         e.preventDefault();
+        debugger
         var name = e.target.player.value;
         console.log(name)
         var results = []
@@ -26,8 +44,10 @@ function drawPlayers(players){
         // tempArr = playersService.getPlayersByPosition(name)
         // results.concat(tempArr)
         console.log(results);
+        drawPlayers(results);
     }
     this.tester = function tester(e) {
+        e.preventDefault();
         var test
         console.log('working')
         test = playersService.getPlayersData();
